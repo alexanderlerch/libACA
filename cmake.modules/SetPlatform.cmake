ff6122dad80e6b60401e0c9bf7647e8fbf642b20
alexanderlerch <alexander.lerch@gatech.edu>
2022-04-30 20:16:37 -0400

    cmake tweaks (hopefully)

--- cmake.modules/SetPlatform.cmake
+++ cmake.modules/SetPlatform.cmake
@@ -1,31 +1,32 @@
 ## set defines for operating system and platform
 
 if (APPLE)
-	set(GTCMT_MACOSX 1)
+	set(ACA_MACOSX 1)
+	add_compile_options("-Wno-c++11")
 endif (APPLE)
 
 if (UNIX AND NOT APPLE)
 	## get bit depth
 	if ( CMAKE_SIZEOF_VOID_P EQUAL 4)
-		set(GTCMT_LINUX32 1)
+		set(ACA_LINUX32 1)
 	else ( CMAKE_SIZEOF_VOID_P EQUAL 4 )
-		set(GTCMT_LINUX64 1)
+		set(ACA_LINUX64 1)
 	endif ( CMAKE_SIZEOF_VOID_P EQUAL 4 )
 endif (UNIX AND NOT APPLE)
 
 if(MINGW)
 	if ( CMAKE_SIZEOF_VOID_P EQUAL 4)
-		set(GTCMT_WIN32 1)
+		set(ACA_WIN32 1)
 	else ( CMAKE_SIZEOF_VOID_P EQUAL 4 )
-		set(GTCMT_WIN64 1)
+		set(ACA_WIN64 1)
 	endif ( CMAKE_SIZEOF_VOID_P EQUAL 4 )
 endif(MINGW)
 
 if (MSVC)
 	if ( CMAKE_CL_64 EQUAL 0 )
-		set(GTCMT_WIN32 1)
+		set(ACA_WIN32 1)
 	else ( CMAKE_CL_64 EQUAL 0 )
-		set(GTCMT_WIN64 1)
+		set(ACA_WIN64 1)
 	endif ( CMAKE_CL_64 EQUAL 0 )
     foreach (flag_var
              CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_C_FLAGS_RELEASE
