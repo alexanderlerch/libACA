06f1644dfe6fe7ee59d76bd238b0f267d057c631
alexanderlerch <alexander.lerch@gatech.edu>
2022-05-30 23:15:23 -0400

    added template memory buffer allocation

--- inc/helper/Vector.h
+++ inc/helper/Vector.h
@@ -12,6 +12,34 @@
 class CVector
 {
 public:
+
+    /*! allocates a float buffer and inits it with zeros
+    \param pfVec (empty pointer, to be allocated)
+    \param iLength number of floats
+    \return
+    */
+    template<typename T>
+    static void alloc(T*& pfVec, long long iLength)
+    {
+        assert(iLength > 0);
+
+        pfVec = new T[iLength];
+
+        assert(pfVec);
+        setZero(pfVec, iLength);
+    }
+
+    /*! frees a float buffer and sets it to zero
+    \param pfVec (empty pointer, to be allocated)
+    \return
+    */
+    template<typename T>
+    static void free(T*& pfVec)
+    {
+        delete[] pfVec;
+        pfVec = 0;
+    }
+
     /*! sets a buffer to zero
     \param ptSrcDest pointer to memory to be modified
     \param iLength  buffer length
