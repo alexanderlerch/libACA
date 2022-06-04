2629c009fb5cc4a91ed225d624367964c891dd57
alexanderlerch <alexander.lerch@gatech.edu>
2022-06-04 17:58:58 -0400

    added new helper functions

--- inc/helper/Matrix.h
+++ inc/helper/Matrix.h
@@ -48,25 +48,6 @@ public:
         ppfMat = 0;
     }
 
-    /*! copies matrix content to another matrix
-    \param ppfDestMat (destination matrix, user allocated)
-    \param ppfSrcMat (source matrix)
-    \param iNumRows number of rows
-    \param iNumCols number of columns
-    \return
-    */
-    template<typename T>
-    static void alloc(T** ppfDestMat, T** ppfSrcMat, int iNumRows, int iNumCols)
-    {
-        assert(ppfDestMat);
-        assert(ppfSrcMat);
-        assert(iNumRows > 0);
-        assert(iNumCols > 0);
-
-        for (auto m = 0; m < iNumRows; m++)
-            CVector::copy(ppfDestMat[m], ppfSrcMat[m], iNumCols);
-    }
-
     /*! multiplies a matrix with a column vector (MAT * VEC)
     \param pfDestColVec resulting (column) vector of length iNumMatRows (to be written, user allocated)
     \param ppfMatrix matrix to be multiplied
@@ -167,6 +148,65 @@ public:
 
     }
 
+    /*! return maximum value in a matrix
+    \param ppfMat matrix to analyze
+    \param iNumRows number of rows in the matrix
+    \param iNumCols number of columns in the matrix
+    \return
+    */
+    static float getMax(float** ppfMat, int iNumRows, int iNumCols)
+    {
+        assert(iNumRows > 0);
+        assert(iNumCols > 0);
+        assert(ppfMat);
+        assert(ppfMat[0]);
+
+        float fGlobalMax = CVectorFloat::getMax(ppfMat[0], iNumCols);
+        for (auto m = 1; m < iNumRows; m++)
+        {
+            float fMax = CVectorFloat::getMax(ppfMat[m], iNumCols);
+            if (fMax > fGlobalMax)
+                fGlobalMax = fMax;
+        }
+        return fGlobalMax;
+    }
+
+    /*! adds a single value to all matrix elements
+    \param ppfMat matrix to analyze
+    \param fAdd scaling factor to apply
+    \param iNumRows number of rows in the matrix
+    \param iNumCols number of columns in the matrix
+    \return
+    */
+    static void addC_I(float** ppfMat, float fAdd, int iNumRows, int iNumCols)
+    {
+        assert(iNumRows > 0);
+        assert(iNumCols > 0);
+        assert(ppfMat);
+        assert(ppfMat[0]);
+
+        for (auto m = 0; m < iNumRows; m++)
+            CVectorFloat::addC_I(ppfMat[m], fAdd, iNumCols);
+    }
+
+    /*! multiplies the whole matrix with a single factor
+    \param ppfMat matrix to analyze
+    \param fScale scaling factor to apply
+    \param iNumRows number of rows in the matrix
+    \param iNumCols number of columns in the matrix
+    \return
+    */
+    static void mulC_I(float** ppfMat, float fScale, int iNumRows, int iNumCols)
+    {
+        assert(iNumRows > 0);
+        assert(iNumCols > 0);
+        assert(ppfMat);
+        assert(ppfMat[0]);
+
+        for (auto m = 0; m < iNumRows; m++)
+            CVectorFloat::mulC_I(ppfMat[m], fScale, iNumCols);
+    }
+
 
     /*! swaps a matrix row with a column
     \param ppfSrcDest resulting matrix (to be modified)
@@ -194,24 +234,60 @@ public:
         }
     }
 
-    /*! copies a matrix 
-    \param ppfDest resulting matrix(to be written, user allocated)
-    \param ppfSrc first matrix to be multiplied
-    \param iNumRows number of rows in both matrices
-    \param iNumCols number of columns in both matrices
+    /*! copies matrix content to another matrix
+    \param ppfDestMat (destination matrix, user allocated)
+    \param ppfSrcMat (source matrix)
+    \param iNumRows number of rows
+    \param iNumCols number of columns
     \return
     */
-    static void copy(float** ppfDest, float** ppfSrc, int iNumRows, int iNumCols)
+    template<typename T>
+    static void copy(T** ppfDestMat, T** ppfSrcMat, int iNumRows, int iNumCols)
     {
+        assert(ppfDestMat);
+        assert(ppfSrcMat);
         assert(iNumRows > 0);
         assert(iNumCols > 0);
-        assert(ppfSrc);
-        assert(ppfSrc[0]);
-        assert(ppfDest);
-        assert(ppfDest[0]);
 
         for (auto m = 0; m < iNumRows; m++)
-            CVectorFloat::copy(ppfDest[m], ppfSrc[m], iNumCols);
+            CVector::copy(ppfDestMat[m], ppfSrcMat[m], iNumCols);
+    }
+
+    /*! copies vector content to a row
+    \param ppfDestMat (destination matrix, user allocated)
+    \param pfSrcVec (source vector)
+    \param iRowIdx index of row to copy to
+    \param iNumCols number of columns
+    \return
+    */
+    template<typename T>
+    static void setRow(T** ppfDestMat, T* pfSrcVec, int iRowIdx, int iNumCols)
+    {
+        assert(ppfDestMat);
+        assert(pfSrcVec);
+        assert(iRowIdx >= 0);
+        assert(iNumCols > 0);
+
+        CVector::copy(ppfDestMat[iRowIdx], pfSrcVec, iNumCols);
+    }
+
+    /*! copies vector content to a column
+    \param ppfDestMat (destination matrix, user allocated)
+    \param pfSrcVec (source vector)
+    \param iColIdx index of row to copy to
+    \param iNumRows number of columns
+    \return
+    */
+    template<typename T>
+    static void setCol(T** ppfDestMat, T* pfSrcVec, int iColIdx, int iNumRows)
+    {
+        assert(ppfDestMat);
+        assert(pfSrcVec);
+        assert(iColIdx >= 0);
+        assert(iNumRows > 0);
+
+        for (auto m = 0; m < iNumRows; m++)
+            ppfDestMat[m][iColIdx] = pfSrcVec[m];
     }
 
     /*! computes inverse of square matrix
