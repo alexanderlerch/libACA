07f96ab380e12ff83fafcef04140d63ebde98392
alexanderlerch <alexander.lerch@gatech.edu>
2022-06-15 14:57:18 -0400

    updates and documents cross validation (resolves #18)

--- src/ACA/ToolLeaveOneOutCrossVal.cpp
+++ src/ACA/ToolLeaveOneOutCrossVal.cpp
@@ -7,13 +7,6 @@
 
 CLeaveOneOutCrossVal::~CLeaveOneOutCrossVal(void) { reset(); }
 
-/*! initializes LeaveOneOutCrossVal instance
-\param iNumFeatures number of features (rows in the input matrix)
-\param iNumObservations number of observations (columns in the feature matrix)
-\param pCClassifier classifier to be used internally
-\return Error_t
-*/
-
 Error_t CLeaveOneOutCrossVal::init(int iNumFeatures, int iNumObservations, CClassifierBase* pCClassifier)
 {
     if (iNumFeatures <= 0 || iNumObservations <= 1)
@@ -31,17 +24,12 @@ Error_t CLeaveOneOutCrossVal::init(int iNumFeatures, int iNumObservations, CClas
     // allocate memory for feature and ground truth data, and query
     CMatrix::alloc(m_ppfTrain, m_iNumFeatures, m_iNumObs - static_cast<long long>(1));
     CVector::alloc(m_piClassLabels, m_iNumObs - static_cast<long long>(1));
+    CVector::alloc(m_pfQuery, m_iNumFeatures);
 
     m_bIsInitialized = true;
 
     return Error_t::kNoError;
 }
-
-/*! initializes LeaveOneOutCrossVal instance
-\param ppfTrainFeatures feature data for 'training' (dimensions iNumFeatures X iNumObservations)
-\param piTrainClassIndices ground truth class index for each observation
-\return Error_t
-*/
 
 float CLeaveOneOutCrossVal::process(float** ppfTrainFeatures, const int* piTrainClassIndices)
 {
@@ -63,11 +51,13 @@ float CLeaveOneOutCrossVal::process(float** ppfTrainFeatures, const int* piTrain
 
     // copy feature data and ground truth data
     CMatrix::copy(m_ppfTrain, ppfTrainFeatures, m_iNumFeatures, m_iNumObs - static_cast<long long>(1));
-    CMatrix::getCol(m_pfQuery, ppfTrainFeatures, m_iNumObs - 1, m_iNumFeatures);
     CVector::copy(m_piClassLabels, piTrainClassIndices, m_iNumObs - static_cast<long long>(1));
 
-    for (int n = 0; n < m_iNumObs; n++)
+    for (int n = 0; n < m_iNumObs-1; n++)
     {
+        CMatrix::getCol(m_pfQuery, ppfTrainFeatures, m_iNumObs - 1 - n, m_iNumFeatures);
+        iGtLabel = piTrainClassIndices[m_iNumObs - 1 - n];
+
         // train (check reset and memory alloc)
         m_pCClassifier->train(m_ppfTrain, m_piClassLabels);
 
@@ -77,10 +67,19 @@ float CLeaveOneOutCrossVal::process(float** ppfTrainFeatures, const int* piTrain
 
         // prep for next round
         CMatrix::setCol(m_ppfTrain, m_pfQuery, m_iNumObs - 2 - n, m_iNumFeatures);
-        CMatrix::getCol(m_pfQuery, ppfTrainFeatures, m_iNumObs - 2 - n, m_iNumFeatures);
         m_piClassLabels[m_iNumObs - 2 - n] = iGtLabel;
-        iGtLabel = piTrainClassIndices[m_iNumObs - 2 - n];
     }
+
+    // last observation (avoid if in loop)
+    CMatrix::getCol(m_pfQuery, ppfTrainFeatures, 0, m_iNumFeatures);
+    iGtLabel = piTrainClassIndices[0];
+
+    // train (check reset and memory alloc)
+    m_pCClassifier->train(m_ppfTrain, m_piClassLabels);
+
+    // classify
+    if (iGtLabel == m_pCClassifier->classify(m_pfQuery))
+        iTruePositives++;
 
     return iTruePositives * 1.F / m_iNumObs;
 }
@@ -96,6 +95,7 @@ Error_t CLeaveOneOutCrossVal::reset()
     // allocate memory for feature and ground truth data, and query
     CMatrix::free(m_ppfTrain, m_iNumFeatures);
     CVector::free(m_piClassLabels);
+    CVector::free(m_pfQuery);
 
     m_iNumFeatures = 0;
     m_iNumObs = 0;
